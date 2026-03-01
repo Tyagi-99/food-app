05c23333b1327f2d1f84da91a58cb1ea69f9fd7d
tyagi-99 <tyagihusain6398@gmail.com>
2026-03-01 15:28:31 +0530

    retain my 50% learning power after revise

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import ReactDOM from "react-dom/client"
 const Header = () =>{
   return (
     <header className="header">
-      <div className="logo">
-        <img className="ImgLogo" src="https://template.canva.com/EAGRMiB2YSc/1/0/1600w-4zJcdIcOrCQ.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUH7DHWAQDT%2F20260228%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260228T055130Z&X-Amz-Expires=22254&X-Amz-Signature=d01adb1a9d033a9a8f4a914c9be4ccfc2bfd91b4d6930c56b81737cdd62ef212&X-Amz-SignedHeaders=host%3Bx-amz-expected-bucket-owner&response-expires=Sat%2C%2028%20Feb%202026%2012%3A02%3A24%20GMT"/>
+      <div className="headerInner">
+        <img className="ImgLogo" src="https://marketplace.canva.com/EAFaFUz4aKo/3/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-tn1zF-_cG9c.jpg"/>
        
       
        <ul className="menu">
@@ -21,9 +21,49 @@ const Header = () =>{
   )
 }
 
+const restaurant = {
+  name: "Al Fahad",
+  cuisine: "Biryani, Mughal, North Indian",
+  location: "Meerut",
+  rating: "4.3",
+  deliveryTime: "44 mins",
+  image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/9ae6376f73c5533cf9edf1f96c051d67"
+};
+
+const ResCard = ({ resData }) => {
+  return (
+    <div className="res-card">
+      <div className="img-container">
+        <img
+          className="res-img"
+          src={resData.image}
+          alt={resData.name}
+        />
+        <div className="rating-badge">⭐ {resData.rating}</div>
+      </div>
+
+      <div className="res-des">
+        <h3 className="res-name">{resData.name}</h3>
+        <p className="res-cuisine">
+          {resData.cuisine}, {resData.location}
+        </p>
+        <p className="res-time">
+          {resData.deliveryTime}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+
+
 const Body = () =>{
   return (
   <div className="body">
+    <ResCard resData={restaurant}/>
+    <ResCard resData={restaurant}/>
+    <ResCard resData={restaurant}/>
+    <ResCard resData={restaurant}/>
 
 
   </div>
